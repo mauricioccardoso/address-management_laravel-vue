f587a8e41cc7a7b47399dd0886eeac2bde680304
Mauricio Cardoso <mauricioerick17@gmail.com>
2023-08-12 23:05:58 -0400

    feat: Adding Bootstrap and configurations

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -6,8 +6,13 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 
+import 'bootstrap/dist/css/bootstrap.min.css'
+import bootstrap from 'bootstrap/dist/js/bootstrap.bundle.js'
+import 'bootstrap-icons/font/bootstrap-icons.css'
+
 const app = createApp(App)
 
+app.use(bootstrap)
 app.use(createPinia())
 app.use(router)
 
