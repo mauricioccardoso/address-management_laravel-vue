46dd1c8e4b18058e6299ead9e500ffae8266f50c
Mauricio Cardoso <mauricioerick17@gmail.com>
2023-08-13 03:47:29 -0400

    feat: Adding address card structure

---
+++ frontend/src/components/AddressCard.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="card h-100">
+    <div class="card-body d-flex flex-column justify-content-between">
+      <div class="mb-3">
+        <h5 class="card-title">{{ street }}</h5>
+        <p class="card-text">{{ neineighborhood }}, {{ city }}/{{ state }} - {{ cep }}</p>
+      </div>
+      <div class="d-flex justify-content-evenly">
+        <button class="btn btn-primary fs-6">
+          Editar
+          <i class="bi bi-pencil-fill fs-"></i>
+        </button>
+        <button class="btn btn-danger">
+          Excluir
+          <i class="bi bi-trash2-fill"></i>
+        </button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+defineProps<{
+  cep: String
+  street: String,
+  neineighborhood: String,
+  city: String,
+  state: String,
+}>();
+</script>
--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,5 +1,32 @@
 <template>
-  <div>
-    <h1>Home View</h1>
+  <div class="row g-2">
+    <div v-for="address in addresses" :key="address.id" class="col-sm-12 col-md-4 col-lg-3">
+      <AddressCard :street="address.street" :neineighborhood="address.neineighborhood" :city="address.city"
+        :state="address.state" :cep="address.cep">
+      </AddressCard>
+    </div>
   </div>
 </template>
+
+<script setup lang="ts">
+import AddressCard from '@/components/AddressCard.vue';
+
+const addresses = [
+  {
+    id: 1,
+    street: 'Rua Teste',
+    neineighborhood: 'Ba',
+    city: 'Cid',
+    state: 'TS',
+    cep: '000000-000',
+  },
+  {
+    id: 2,
+    street: 'Rua Teste 2',
+    neineighborhood: 'Bairro Teste 2',
+    city: 'Cidade Teste 2',
+    state: 'TS',
+    cep: '000000-001',
+  }
+]
+</script>
